89f983d7f6b482b5e8f0a82894c3858faab68b68
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-07-28 18:15:58 +0530

    Create maintestcases.c

---
+++ maintestcases.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include <stdio.h>
+#include "BatteryManagementSystem.h"
+
+int main() {
+    assert(batteryIsOk(25, 70, 0.7));
+    assert(!batteryIsOk(50, 85, 0));
+    assert(batteryIsOk(5, 25, 0.7));  // Test cases for warnings
+    assert(batteryIsOk(43, 75, 0.75)); // Test cases for warnings
+    assert(!batteryIsOk(-1, 25, 0.7)); // Test cases for out of range
+    assert(!batteryIsOk(25, 85, 0.7)); // Test cases for out of range
+    assert(!batteryIsOk(25, 25, 0.9)); // Test cases for out of range
+
+    printf("All tests passed.\n");
+    return 0;
+}
