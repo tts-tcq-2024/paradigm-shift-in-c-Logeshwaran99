a65c9e1f88e60c85512aa24e3f88e39f296c9a06
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-07-28 18:14:40 +0530

    Update BatteryManagementSystem.h

--- BatteryManagementSystem.h
+++ BatteryManagementSystem.h
@@ -1,31 +1,12 @@
-#ifndef BATTERYMANAGEMENTSYSTEM_H
-#define BATTERYMANAGEMENTSYSTEM_H
+#ifndef BATTERY_MANAGEMENT_SYSTEM_H
+#define BATTERY_MANAGEMENT_SYSTEM_H
 
+#include <stdbool.h>
+#include "RangeCheck.h"
+#include "TemperatureCheck.h"
+#include "StateOfChargeCheck.h"
+#include "ChargeRateCheck.h"
 
-// Constants for thresholds and tolerances
-#define TEMPERATURE_MAX 45
-#define TEMPERATURE_MIN 0
-#define SOC_MAX 80
-#define SOC_MIN 20
-#define CHARGE_RATE_MAX 0.8
-
-#define TOLERANCE_PERCENTAGE 5
-
-#define TEMPERATURE_WARNING_MAX (TEMPERATURE_MAX - (TEMPERATURE_MAX * TOLERANCE_PERCENTAGE / 100))
-#define TEMPERATURE_WARNING_MIN (TEMPERATURE_MIN + (TEMPERATURE_MIN * TOLERANCE_PERCENTAGE / 100))
-#define SOC_WARNING_MAX (SOC_MAX - (SOC_MAX * TOLERANCE_PERCENTAGE / 100))
-#define SOC_WARNING_MIN (SOC_MIN + (SOC_MIN * TOLERANCE_PERCENTAGE / 100))
-#define CHARGE_RATE_WARNING_MAX (CHARGE_RATE_MAX - (CHARGE_RATE_MAX * TOLERANCE_PERCENTAGE / 100))
-
-typedef enum {
-    OK,
-    WARNING,
-    ERROR
-} Status;
-
-Status checkTemperature(float temperature);
-Status checkStateOfCharge(float soc);
-Status checkChargeRate(float chargeRate);
 bool batteryIsOk(float temperature, float soc, float chargeRate);
 
-#endif /*BATTERYMANAGEMENTSYSTEM_H */
+#endif // BATTERY_MANAGEMENT_SYSTEM_H
