2c10fada56817fdfdb8cbf447a3896b1efec8dd3
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-07-28 18:12:26 +0530

    Create TemperatureCheck.c

---
+++ TemperatureCheck.c
@@ -0,0 +1,16 @@
+#include "TemperatureCheck.h"
+#include "RangeCheck.h"
+
+// Constants for temperature thresholds and tolerances
+#define TEMPERATURE_MAX 45
+#define TEMPERATURE_MIN 0
+#define TOLERANCE_PERCENTAGE 5
+#define TEMPERATURE_WARNING_MAX (TEMPERATURE_MAX - (TEMPERATURE_MAX * TOLERANCE_PERCENTAGE / 100))
+#define TEMPERATURE_WARNING_MIN (TEMPERATURE_MIN + (TEMPERATURE_MIN * TOLERANCE_PERCENTAGE / 100))
+
+Status checkTemperature(float temperature) {
+    if (checkOutOfRange(temperature, TEMPERATURE_MIN, TEMPERATURE_MAX, "Temperature out of range!") == ERROR) {
+        return ERROR;
+    }
+    return checkApproachingLimit(temperature, TEMPERATURE_WARNING_MIN, TEMPERATURE_WARNING_MAX, "Warning: Temperature approaching limit!");
+}
